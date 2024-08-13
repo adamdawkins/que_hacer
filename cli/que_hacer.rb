0917f5df53c8fc1cf41acc0a291f5a86d6cf0f4d
adamdawkins <adam@dragondrop.uk>
2024-08-13 21:52:13 +0100

    Use TodosRepository#complete from CLI
    This business logic snuck into the CLI, it was functional, but I'd have
    to do it in every place in the shell(s) we need to do it. This is
    better. Made clear by 'remove'

--- cli/que_hacer.rb
+++ cli/que_hacer.rb
@@ -3,7 +3,6 @@ require "bundler/setup"
 require "dry/cli"
 
 require_relative "../core/todos_repository"
-require_relative "../core/actions/todos/complete"
 require_relative "../persistence/fetch"
 require_relative "../persistence/save"
 
@@ -71,15 +70,7 @@ module QueHacer
           ]
 
           def call(id:, **)
-            @todos ||= TodosRepository.new(Persistence::Fetch.new.all).all
-            @todos = @todos.each_with_index.map do |todo, index|
-              if index == id.to_i
-                Actions::Todos::Complete.call(todo)
-              else
-                todo
-              end
-            end
-
+            @todos ||= TodosRepository.new(Persistence::Fetch.new.all).complete(id.to_i)
             Persistence::Save.new.call(@todos)
 
             @todos.each do |todo, index|
